75951e861f2a8d740c32344d74c70e236f4228d2
as <12@ex.com>
2016-07-22 10:57:05 +0800

    Activity生命周期

---
+++ src/main/java/com/w/activitylife/Activitysecond.java
@@ -0,0 +1,9 @@
+package com.w.activitylife;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class Activitysecond extends AppCompatActivity {
+}
