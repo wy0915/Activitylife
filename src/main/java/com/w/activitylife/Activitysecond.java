9bcb9ea0fc671b0e34203a25fdcd7c037ec63b25
as <12@ex.com>
2016-07-23 10:38:49 +0800

    传递数据7月23日

--- src/main/java/com/w/activitylife/Activitysecond.java
+++ src/main/java/com/w/activitylife/Activitysecond.java
@@ -1,9 +1,23 @@
 package com.w.activitylife;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 public class Activitysecond extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        String inf = getIntent().getStringExtra(MainActivity.BOOKMAME_OF_ENGLISH);
+        textView = (TextView) findViewById(R.id.second_activity_text_view);
+        textView.setText(inf);
+    }
 }
--- src/main/java/com/w/activitylife/MainActivity.java
+++ src/main/java/com/w/activitylife/MainActivity.java
@@ -7,23 +7,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public static final String BOOKMAME_OF_ENGLISH="bookName_of_English";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate执行了");
         Button button = (Button) findViewById(R.id.activity_main_button);
+        final EditText edit = (EditText) findViewById(R.id.activity_main_edit_text);
         assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Intent intent = new Intent(MainActivity.this, Activitysecond.class);
 //                startActivity(intent);
-                Toast.makeText(MainActivity.this, "单击过按钮了", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "单击过按钮了", Toast.LENGTH_SHORT).show();
+            String data ="跳转";
+                Intent intent = new Intent(MainActivity.this, Activitysecond.class);
+                intent.putExtra(BOOKMAME_OF_ENGLISH,edit.getText().toString());
+                startActivity(intent);
             }
         });
     }
@@ -63,4 +70,6 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.d(TAG, "onRestart: 执行了");
     }
+
+
 }
